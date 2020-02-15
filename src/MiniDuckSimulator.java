a529193425a1297821e255e74253dce35af6f516
Matthew Philps <philps.matthew@gmail.com>
2020-02-15 21:19:58 +0900

    added model duck and rocket powered flight

--- src/MiniDuckSimulator.java
+++ src/MiniDuckSimulator.java
@@ -3,5 +3,11 @@ public class MiniDuckSimulator {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
+
+        Duck model = new ModelDuck();
+        model.performQuack();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
     }
 }
